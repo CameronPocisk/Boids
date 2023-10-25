7959331d6b07e8216ea488ed7278642877a31d51
CameronPocisk <CameronPocisk@gmail.com>
2023-10-24 22:51:11 -0400

    teeny start

--- Boids.js
+++ Boids.js
@@ -1,1 +1,11 @@
 // I can get behind Javascript
+class Boid{
+    constructor(xPosition, yPosition) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+      }
+}
+
+const firstBoid = new Boid(25, 50);
+
+console.log(firstBoid.xPosition);
