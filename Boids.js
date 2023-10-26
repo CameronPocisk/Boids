d2ca8b097401b24a2062962c6e7cc45f74987c60
CameronPocisk <CameronPocisk@gmail.com>
2023-10-26 00:01:40 -0400

    Triangle spawn-in 10/26

--- Boids.js
+++ Boids.js
@@ -1,11 +1,103 @@
-// I can get behind Javascript
+// Boid Rules
+const seperaion = false;
+const alignment = false;
+const cohesion = false;
+
+const vertexData = [
+  0, 1, 0,
+  1, -1, 0,
+  -1, -1, 0,
+];
 class Boid{
-    constructor(xPosition, yPosition) {
-        this.xPosition = xPosition;
-        this.yPosition = yPosition;
-      }
+
+  angle = 90; // Degrees
+
+  xPosition;
+  yPosition;
+
+  xVelocity;
+  yVelocity;
+  vectorVelocity;
+
+  awarenessCircleRadius;
+
+  // Triangle dimensions (all isococilies)
+  base;
+  height;
+  color; // Hextstring?
+
+  constructor(xPosition) {
+      this.xPosition = xPosition;
+      this.yPosition;
+  }
+
+  setXPosition(newXPosition){
+    this.x = newXPosition;
+  }
+
+  DrawBoid(){
+  }
+
+  Update(){
+  }
 }
 
-const firstBoid = new Boid(25, 50);
+function main(){
 
-console.log(firstBoid.xPosition);
+  const canvas = document.querySelector('canvas'); // const canvas = document.getElementById('boidPlane');
+  const gl = canvas.getContext('webgl');
+
+  if (!gl){
+    throw new Error('WebGL not supported')
+  }
+
+  // Create a black canvas
+  gl.clearColor(.2, .2, .3, 1); // full opaque dark purple bkg
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  
+  // creating buffer
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer) // means that the buffer is an array of vertices
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW) // loads data into the array buffer and how often we redraw
+
+  // creating vertex shader
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER); // A shader is like a mini program and it needs some code to run
+  gl.shaderSource(vertexShader, `
+  attribute vec3 position;
+  void main() {
+    gl_Position = vec4(position, 2);
+  }
+  `); // The code it runs (its not javascript, its the gl shading language which runs on the GPU)
+  gl.compileShader(vertexShader); // compiles the shader
+
+
+  // creating fragment shader
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, `
+  void main() {
+    gl_FragColor = vec4(.8, .75, .60, 1);
+  }
+  `);
+  gl.compileShader(fragmentShader);
+
+  // creating program
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  // enabling vertex attributes (disabled by default)
+  const positionLocation = gl.getAttribLocation(program, 'position'); // this get the number that webGL has assigned to the attribute in the shaders
+  gl.enableVertexAttribArray(positionLocation);
+  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0) // describe how webGL should retrieve attribute data from the buffer
+
+  gl.useProgram(program); // will create the executable program on the GPU
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+  const firstBoid = new Boid(25);
+  firstBoid.xPosition = 6; // Public??????
+  console.log(firstBoid.xPosition);
+
+}
+
+main();
--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Cameron Pocisk and Cristian Leyva's Repo for Boids Project.
 We have decided to do the project in Javascript using WebGL. 
 This is becuase the end goal is to make this a customizable wallpaper on Wallpaper engine. 
 Wallpaper engine supports interactive webpages (javascript!) as desktops. 
+
+10/25/2023
+Helpful doc for html canvas https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+openGL is a state machine -- from (https://youtu.be/kB0ZVUrI4Aw?si=XxpGntDI3RzrBVSu&t=149)
+Using webGL https://www.youtube.com/playlist?list=PL2935W76vRNHFpPUuqmLoGCzwx_8eq5yK
