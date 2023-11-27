d8a939db96ff90de44ad984524173ad50217451d
Cameron Pocisk <cameron.pocisk@geappliances.com>
2023-11-26 19:48:25 -0500

    Working triangle maker woot woot

--- withCanvas/test.js
+++ withCanvas/test.js
@@ -1,8 +1,6 @@
 // Made by Cameron Pocisk and Cristian Leyva
 
 // Globals and Constants
-var WINDOW_HEIGHT = window.innerWidth; // Add an event listener and make these plastic
-var WINDOW_WIDTH = window.innerHeight;
 // bools for - seperation, allignment, cohesion, WrapAround
 const canvas = document.getElementById("boidPlane");
 const context = canvas.getContext("2d");
@@ -11,8 +9,8 @@ class Boid{
 
     angle; // Radians looooool (for Math)
 
-    xPosition = WINDOW_WIDTH / 2; // These represent the Center of the boid
-    yPosition = WINDOW_HEIGHT / 2; // Starts at the center of screen by default
+    xPosition = window.width / 2; // These represent the Center of the boid
+    yPosition = window.height / 2; // Starts at the center of screen by default
 
     xVelocity;
     yVelocity;
@@ -21,7 +19,7 @@ class Boid{
 
     // Triangle dimensions (all isococilies)
     base = window.innerWidth / 15;
-    height = window.innerHeight / 10;
+    height = window.innerHeight / 8;
     color = "#000000"; // Hexstring?
 
     constructor(xPosInp, yPosInp, angleInp) {
@@ -32,21 +30,21 @@ class Boid{
 
     DrawBoid()
     { // Draws from the center of the boid
-        context.beginPath();
+        context.beginPath(); // I think that I need to add to account for the bias of (0,0) being the top left
+        // I think that the best way to handle this is to treat it like the top rigjt quadrant
         context.moveTo(this.xPosition + (Math.cos(this.angle) * this.height * 1/2),
         this.yPosition + (Math.sin(this.angle) * this.height * 1/2)); // Top of triangle
 
-        context.lineTo(this.xPosition + (-1/2((Math.cos(this.angle) * this.height) - (Math.sin(this.angle) * this.base))),
-        this.yPosition + (-1/2((Math.sin(this.angle) * this.height) + (Math.cos(this.angle) * this.base))));
+        context.lineTo(this.xPosition + (-1/2 * ((Math.cos(this.angle) * this.height) - (Math.sin(this.angle) * this.base))),
+        this.yPosition + (-1/2 * ((Math.sin(this.angle) * this.height) + (Math.cos(this.angle) * this.base))));
 
-        context.lineTo(this.xPosition + (-1/2((Math.cos(this.angle) * this.height) + (Math.sin(this.angle) * this.base))),
-        this.yPosition + (-1/2((Math.sin(this.angle) * this.height) - (Math.cos(this.angle) * this.base))));
+        context.lineTo(this.xPosition + (-1/2 * ((Math.cos(this.angle) * this.height) + (Math.sin(this.angle) * this.base))),
+        this.yPosition + (-1/2 * ((Math.sin(this.angle) * this.height) - (Math.cos(this.angle) * this.base))));
         context.fill();
-    }
 
-    Update(){
+        console.log(this.xPosition + (Math.cos(this.angle) * this.height * 1/2));
     }
-  }
+};
 
 function main()
 {
@@ -58,14 +56,14 @@ function main()
 
     context.beginPath();
     // X, y Quad III
-    context.moveTo(WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
-    context.lineTo(WINDOW_WIDTH/2 -15, WINDOW_HEIGHT/2 + 40);
-    context.lineTo(WINDOW_WIDTH/2 +15, WINDOW_HEIGHT/2 + 40);
-    context.fill();
+    // context.moveTo(window.innerWidth/2, window.innerHeight/2 );
+    // context.lineTo(window.innerWidth/2 -100, window.innerHeight/2 +100);
+    // context.lineTo(window.innerWidth/2 + 100, window.innerHeight/2 +100);
+    // context.fill();
 
     // New test triangle using boid
-    const firstBoid = new Boid(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, Math.PI);
-    firstBoid.DrawBoid;
+    const firstBoid = new Boid(window.innerWidth/2, window.innerHeight/2, Math.PI/2);
+    firstBoid.DrawBoid();
 }
 
 main();
