2b4fdf7ba3509303068b4680039d3d0745c6fddf
CameronPocisk <cameronpocisk@gmail.com>
2024-02-06 16:36:56 -0500

    more movement stuff in vector format i think ill add acceleration next

--- Boids.js
+++ Boids.js
@@ -2,8 +2,8 @@
 
 // Globals and Constants
 const FrameRateInMsec = Math.floor(1/60);
-var BoidHeight = window.innerHeight / 4; // Var bc I think these will have to be plastic
-var boidWidth = window.innerWidth / 15;
+var BoidHeight = window.innerHeight / 8; // Var bc I think these will have to be plastic
+var boidWidth = window.innerWidth / 30;
 // bools for - seperation, allignment, cohesion, WrapAround
 const canvas = document.getElementById("boidPlane");
 const context = canvas.getContext("2d");
@@ -27,12 +27,15 @@ class Boid{
     xPosition = window.width / 2; // These represent the Center of the boid
     yPosition = window.height / 2; // Starts at the center of screen by default
 
-    xVelocity;
+    velocity;
+    // Used to save space and time but always derived from velocity
+    xVelocity; 
     yVelocity;
 
-    constructor(xPosInp, yPosInp, angleInp) {
+    constructor(xPosInp, yPosInp, velocityInp, angleInp) {
         this.xPosition = xPosInp;
         this.yPosition = yPosInp;
+        this.velocity = velocityInp;
         this.angle = angleInp;
     }
 
@@ -50,14 +53,28 @@ class Boid{
     {   // Math for drawing an isocolese triangle from its center given its X, Y, Base, Height, Angle.
         this.firstXPoint = this.xPosition + (Math.cos(this.angle) * BoidHeight * 1/2);
         this.firstYPoint = this.yPosition + (Math.sin(this.angle) * BoidHeight * 1/2);
-        this.secondXPoint =this.xPosition + (-1/2 * ((Math.cos(this.angle) * BoidHeight) - (Math.sin(this.angle) * boidWidth)));
-        this.secondYPoint =this.yPosition + (-1/2 * ((Math.sin(this.angle) * BoidHeight) + (Math.cos(this.angle) * boidWidth)));
+        this.secondXPoint= this.xPosition + (-1/2 * ((Math.cos(this.angle) * BoidHeight) - (Math.sin(this.angle) * boidWidth)));
+        this.secondYPoint= this.yPosition + (-1/2 * ((Math.sin(this.angle) * BoidHeight) + (Math.cos(this.angle) * boidWidth)));
         this.thirdXPoint = this.xPosition + (-1/2 * ((Math.cos(this.angle) * BoidHeight) + (Math.sin(this.angle) * boidWidth)));
         this.thirdYPoint = this.yPosition + (-1/2 * ((Math.sin(this.angle) * BoidHeight) - (Math.cos(this.angle) * boidWidth)));
     }
 
+    GetVelocityComponents()
+    {
+        this.xVelocity = Math.cos(this.angle) * this.velocity;
+        this.yVelocity = Math.sin(this.angle) * this.velocity;
+    }
+
+    MoveWithVelocity()
+    {
+        this.xPosition += this.xVelocity;
+        this.yPosition += this.yVelocity;
+    }
+
     Update()
     {
+        this.GetVelocityComponents();
+        this.MoveWithVelocity();
         this.UpdateTriangleCoordinates();
         this.DrawBoid();
     }
@@ -72,14 +89,25 @@ function main()
     canvas.style.border = "1px solid #FF0000";
     window.addEventListener("resize", reportWindowSize);
 
-    const firstBoid = new Boid(window.innerWidth/2, window.innerHeight/2, Math.PI/2);
+    const firstBoid = new Boid(window.innerWidth/2, window.innerHeight/2, 5, Math.PI/2);
     setInterval(() => {
         context.clearRect(0, 0, canvas.width, canvas.height); // Move this outside of class, This has to happen upon each 'frame'
-        firstBoid.angle += .015;
+        
+        firstBoid.angle += Math.random() / 10;
 
-        firstBoid.xPosition += 2;
         if(firstBoid.xPosition > window.innerWidth){
             firstBoid.xPosition = 0;
+        }
+        else if(firstBoid.xPosition < 0)
+        {
+            firstBoid.xPosition = window.innerWidth
+        }
+        if(firstBoid.yPosition > window.innerHeight){
+            firstBoid.yPosition = 0;
+        }
+        else if(firstBoid.yPosition < 0)
+        {
+            firstBoid.yPosition = window.innerHeight
         }
 
         firstBoid.Update();
