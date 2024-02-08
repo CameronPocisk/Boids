ec4a077c6ebcac42c687ebf9b4831b810e6e500f
CameronPocisk <CameronPocisk@gmail.com>
2024-02-07 21:23:29 -0500

    nice changes

--- Boids.js
+++ Boids.js
@@ -2,9 +2,9 @@
 
 // Globals and Constants
 const FrameRateInMsec = 1/60;
-var BoidHeight = window.innerHeight / 8; // Var bc I think these will have to be plastic
-var boidWidth = window.innerWidth / 30;
-const numBoids = 2;
+var BoidHeight = window.innerHeight / 16; // Var bc I think these will have to be plastic
+var boidWidth = window.innerWidth / 60;
+const numBoids = 99;
 // bools for - seperation, allignment, cohesion, WrapAround
 const canvas = document.getElementById("boidPlane");
 const context = canvas.getContext("2d");
@@ -19,7 +19,7 @@ function SetupCanvas(){
 }
 
 function ReportWindowSize(){
-    BoidHeight = window.innerHeight / numBoids;
+    BoidHeight = window.innerHeight / 8;
     boidWidth = window.innerWidth / 15;
 }
 function RandomNumberBetween(min, max){
@@ -32,6 +32,8 @@ class Boid{
     yPosition;
     velocity;
     angle; // Radians looooool (for Math)
+    sinVelocityFactor;
+    cosVelocityFactor;
 
     firstXPoint; // Triangle things
     firstYPoint;
@@ -75,26 +77,32 @@ class Boid{
         }
     }
 
+    CalculateTrigAngleFactors()
+    {
+        this.sinVelocityFactor = Math.sin(this.angle);
+        this.cosVelocityFactor = Math.cos(this.angle);
+    }
+
     UpdateTriangleCoordinates()
     {   // Math for drawing an isocolese triangle from its center given its X, Y, Base, Height, Angle.
         // Worth performance wise to save sin and cos' as vars? (5 each per frame) (Do Later)
-        this.firstXPoint = this.xPosition + (Math.cos(this.angle) * BoidHeight * 1/2);
-        this.firstYPoint = this.yPosition + (Math.sin(this.angle) * BoidHeight * 1/2);
-        this.secondXPoint= this.xPosition + (-1/2 * ((Math.cos(this.angle) * BoidHeight) - (Math.sin(this.angle) * boidWidth)));
-        this.secondYPoint= this.yPosition + (-1/2 * ((Math.sin(this.angle) * BoidHeight) + (Math.cos(this.angle) * boidWidth)));
-        this.thirdXPoint = this.xPosition + (-1/2 * ((Math.cos(this.angle) * BoidHeight) + (Math.sin(this.angle) * boidWidth)));
-        this.thirdYPoint = this.yPosition + (-1/2 * ((Math.sin(this.angle) * BoidHeight) - (Math.cos(this.angle) * boidWidth)));
+        this.firstXPoint = this.xPosition + (this.cosVelocityFactor * BoidHeight * 1/2);
+        this.firstYPoint = this.yPosition + (this.sinVelocityFactor * BoidHeight * 1/2);
+        this.secondXPoint= this.xPosition + (-1/2 * ((this.cosVelocityFactor * BoidHeight) - this.sinVelocityFactor * boidWidth));
+        this.secondYPoint= this.yPosition + (-1/2 * ((this.sinVelocityFactor * BoidHeight) + this.cosVelocityFactor * boidWidth));
+        this.thirdXPoint = this.xPosition + (-1/2 * ((this.cosVelocityFactor * BoidHeight) + this.sinVelocityFactor * boidWidth));
+        this.thirdYPoint = this.yPosition + (-1/2 * ((this.sinVelocityFactor * BoidHeight) - this.cosVelocityFactor * boidWidth));
     }
 
     MoveWithVelocity()
     {
-        this.xPosition += Math.cos(this.angle) * this.velocity;
-        this.yPosition += Math.sin(this.angle) * this.velocity;
+        this.xPosition += this.cosVelocityFactor * this.velocity;
+        this.yPosition += this.sinVelocityFactor * this.velocity;
     }
 
     Update()
     {
-        
+        this.CalculateTrigAngleFactors();
         this.MoveWithVelocity();
         this.CheckForOutOfBounds();
         this.UpdateTriangleCoordinates();
@@ -111,7 +119,7 @@ function main()
     for(let i = 0; i < numBoids; i++){
         aFewBoids.push(new Boid(RandomNumberBetween(0, window.innerWidth), 
         RandomNumberBetween(0, window.innerHeight),
-        RandomNumberBetween(2, 6),
+        RandomNumberBetween(2, 3),
         Math.PI/2));
     }
     var frameCount = 0
@@ -124,6 +132,7 @@ function main()
         
         for(let i = 0; i < numBoids; i++){ // Move into class?
             aFewBoids[i].angle += RandomNumberBetween(-.02, .02);
+            // aFewBoids[i].angle += .01;
         }
         
         // Do a majority of the work for boid updating
