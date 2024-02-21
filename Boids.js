27c4301178a653e997e355e501986a0bea78e4c7
CameronPocisk <cameronpocisk@gmail.com>
2024-02-20 23:04:44 -0500

    added move away functionlity and getting nearby boids

--- Boids.js
+++ Boids.js
@@ -8,7 +8,8 @@ var mouseYPosition = 0;
 
 // Helpful constants
 const FrameRateInMsec = (1/60) * 1000;
-const numBoids = 20;
+const aFewBoids = [];
+const numBoids = 5;
 
 // bools for - seperation, allignment, cohesion, WrapAround?
 
@@ -38,32 +39,38 @@ function SetupCanvas(){
 function RandomNumberBetween(min, max){
     return min + Math.random() * (max - min);
 }
+function DistanceBetweenPoints(x1, y1, x2, y2){
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
 
 class Boid{
 
+    static distanceToAvoid = 200;
     static widthOfBoid;
     static heightOfBoid;
     static FeildOfView = Math.PI // When the boid will scan to find empty space
-
+    
     xPosition;
     yPosition;
     velocity;
     angle; // Radians looooool (for Math)
     sinOfBoidAngle;
     cosOfBoidAngle;
-
+    
     firstXPoint; // Triangle things
     firstYPoint;
     secondXPoint;
     secondYPoint;
     thirdXPoint;
     thirdYPoint;
-
+    
     constructor(xPosInp, yPosInp, velocityInp, angleInp) {
         this.xPosition = xPosInp;
         this.yPosition = yPosInp;
         this.velocity = velocityInp;
         this.angle = angleInp;
+        this.distanceToAvoid = 200;
+        this.nearbyBoids = []; // adds n
     }
 
     DrawBoid()
@@ -112,11 +119,10 @@ class Boid{
     RandomAngleChange(){
         this.angle += RandomNumberBetween(-.1, .1);
     }
-    
-    MoveTowardsCursor()
-    {
-        const relativeXPosition = mouseXPosition - this.xPosition;
-        const relativeYPosition = -1*(mouseYPosition - this.yPosition);
+
+    MoveToCoords(coordX, coordY){
+        const relativeXPosition = coordX - this.xPosition;
+        const relativeYPosition = -1*(coordY - this.yPosition);
         var angleFromBoid = Math.atan(relativeYPosition / relativeXPosition);
 
         if (relativeXPosition < 0){ // Handle weird Arctangent outputs
@@ -133,10 +139,71 @@ class Boid{
             this.angle -= .07; // Turn right
         }
     }
+    MoveAwayFromCoords(coordX, coordY){
+        const relativeXPosition = coordX - this.xPosition;
+        const relativeYPosition = -1*(coordY - this.yPosition);
+        var angleFromBoid = Math.atan(relativeYPosition / relativeXPosition);
 
-    DrawLineToAllBoids(constBoidArray){
-        
+        if (relativeXPosition < 0){ // Handle weird Arctangent outputs
+            angleFromBoid += Math.PI;
+        }
+        else if(relativeYPosition < 0){ // 4th quad will range from 270 - 360 with this
+            angleFromBoid += Math.PI*2;
+        }
+        // console.log("Boid in deg: " + this.angle * 180 / Math.PI); console.log("Angle from : " + angleFromBoid * 180 / Math.PI);
+        if((angleFromBoid > this.angle && angleFromBoid < this.angle + Math.PI) || angleFromBoid < this.angle - Math.PI){
+            this.angle -= .07; // Turn left
+        }
+        else{
+            this.angle += .07; // Turn right
+        }
     }
+    
+    MoveTowardsCursor()
+    {
+        this.MoveToCoords(mouseXPosition, mouseYPosition);
+    }
+
+    MoveAwayFromObjectIfClose(objX, objY){  //distanceToAvoid
+        if(DistanceBetweenPoints(this.xPosition, this.yPosition, objX, objY) < 200){
+            this.MoveAwayFromCoords(objX, objY);
+        }
+    }
+
+    DrawLineToAllBoids(aFewBoids){
+        for(let i = 0; i < aFewBoids.length; i++){
+            context.beginPath();
+            context.moveTo(this.xPosition, this.yPosition);
+            context.lineTo(aFewBoids[i].xPosition, aFewBoids[i].yPosition);
+            context.stroke();
+        }
+    }
+    DrawLineToNearbyBoids(){
+        for(let i = 0; i < this.nearbyBoids.length; i++){
+            context.beginPath();
+            context.moveTo(this.xPosition, this.yPosition);
+            context.lineTo(this.nearbyBoids[i].xPosition, this.nearbyBoids[i].yPosition);
+            context.fillStyle = "#FF0000";
+            context.stroke();
+        }
+    }
+
+    GetArrayOfNearbyBoidsFromAll(boidArrayIn){
+        var nearbyBoids = [];
+        for(let i = 0; i < boidArrayIn.length; i++){
+            var dist = DistanceBetweenPoints(this.xPosition, this.yPosition, boidArrayIn[i].xPosition, boidArrayIn[i].yPosition);
+            if(DistanceBetweenPoints() < this.distanceToAvoid){
+                nearbyBoids.push(boidArrayIn[i]);
+            }
+        }
+    }
+
+    MoveAwayFromNearbyBoids(){ // Will this work well without a formula for how much angle to change?
+        for(let i = 0; i < this.nearbyBoids.length; i++){
+            this.MoveAwayFromCoords(this.nearbyBoids[i].xPosition, this.nearbyBoids[i].yPosition);
+        }
+    }
+
 
     Update()
     {
@@ -144,8 +211,14 @@ class Boid{
         this.CalculateTrigAngleFactors();
         
         // Handle angle
-        this.MoveTowardsCursor(); // Add fun thing for mouse off screen?
-        // this.RandomAngleChange();
+        // this.MoveTowardsCursor(); // Add fun thing for mouse off screen?
+        // this.MoveAwayFromObjectIfClose(mouseXPosition, mouseYPosition);
+        this.RandomAngleChange();
+
+        this.GetArrayOfNearbyBoidsFromAll(aFewBoids);
+        this.DrawLineToNearbyBoids();
+        
+        this.MoveAwayFromNearbyBoids();
 
         // Handle Movement
         this.MoveWithVelocity();
@@ -163,20 +236,19 @@ function main()
     SetupCanvas();
 
     // Initialize boids
-    const aFewBoids = [];
 
-    // for(let i = 0; i < numBoids; i++){
-    //     aFewBoids.push(new Boid(RandomNumberBetween(0, window.innerWidth), 
-    //     RandomNumberBetween(0, window.innerHeight),
-    //     RandomNumberBetween(7, 8),
-    //     Math.PI/2));
-    // }
     for(let i = 0; i < numBoids; i++){
-        aFewBoids.push(new Boid(window.innerWidth/2, 
-        window.innerHeight/2,
-        RandomNumberBetween(0, 0),
+        aFewBoids.push(new Boid(RandomNumberBetween(0, window.innerWidth), 
+        RandomNumberBetween(0, window.innerHeight),
+        RandomNumberBetween(7, 8),
         Math.PI/2));
     }
+    // for(let i = 0; i < numBoids; i++){
+    //     aFewBoids.push(new Boid(window.innerWidth/2, 
+    //     window.innerHeight/2,
+    //     RandomNumberBetween(0, 0),
+    //     Math.PI/2));
+    // }
 
     // Start main loop
     var frameCount = 0;
@@ -189,7 +261,9 @@ function main()
         
         // Do a majority of the work for boid updating
         for(let i = 0; i < numBoids; i++){
+            aFewBoids[i].GetArrayOfNearbyBoidsFromAll(aFewBoids);
             aFewBoids[i].Update();
+            // aFewBoids[i].DrawLineToAllBoids(aFewBoids);
         }
 
         // console.log("Ran frame: " + frameCount);
